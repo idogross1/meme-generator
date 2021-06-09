dc5bb7f092e4a130004ed560e23a2241a9e1ab59
idogross1 <idogross1@gmail.com>
2021-06-09 13:02:57 +0300

    added image and text

--- js/controller.js
+++ js/controller.js
@@ -6,21 +6,30 @@ var gCtx;
 function init() {
   gElCanvas = document.getElementById('canvas');
   gCtx = canvas.getContext('2d');
-  renderImage();
+  //   renderText(gMeme.lines[0].txt);
+  renderMeme(gMeme.selectedImgId);
 }
 
-function renderImage(imageId = 15) {
+function renderMeme(memeId) {
   var img = new Image();
-  img.src = `../images/${imageId}.jpg`;
-  //   console.log(img);
-  console.log(gCtx);
-  console.log(gElCanvas);
+  img.src = `../images/${memeId}.jpg`;
   img.onload = () => {
-    var w = canvas.width;
-    var nw = img.naturalWidth;
-    var nh = img.naturalHeight;
-    var h = (w * nh) / nw;
-    console.log(h);
-    gCtx.drawImage(img, 0, 0, w, h);
+    renderImage(img);
+    renderText(gMeme.lines[0].txt);
   };
+}
+
+function renderImage(img) {
+  var w = canvas.width;
+  var nw = img.naturalWidth;
+  var nh = img.naturalHeight;
+  var h = (w * nh) / nw;
+  gCtx.drawImage(img, 0, 0, w, h);
+}
+
+function renderText(txt) {
+  gCtx.lineWidth = 2;
+  gCtx.font = '40px Impact';
+  gCtx.textBaseline = 'middle';
+  gCtx.fillText(txt, 20, 100);
 }
